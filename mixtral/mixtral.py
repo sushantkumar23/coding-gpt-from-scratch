bd0a6b815d76cd595ed65e8e82547595f940c516
Sushant Kumar <sushantkumar23@gmail.com>
2024-01-11 14:29:27 +0530

    Added model initilization to the code

--- mixtral/mixtral.py
+++ mixtral/mixtral.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from dataclasses import dataclass
 
 import mlx.core as mx
+import mlx.nn as nn
+from mlx.utils import tree_unflatten
 
 
 @dataclass
@@ -21,6 +23,56 @@ class ModelArgs:
     vocab_size: int
     moe: dict = None
 
+
+class RoPE(nn.RoPE):
+
+    def __init__(self, dims: int, traditional: bool = False):
+        super().__init__(dims, traditional=traditional)
+
+
+class Attention(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.args = args
+
+        self.n_heads: int = args.n_heads
+        self.n_kv_heads: int = args.n_kv_heads
+
+        self.repeats = self.n_heads // self.n_kv_heads
+
+        self.scale = self.args.head_dim ** -0.5
+
+        self.wq = nn.Linear(input_dims=args.dim, output_dims=args.n_heads * args.head_dim, bias=False)
+        self.wk = nn.Linear(input_dims=args.dim, output_dims=args.n_kv_heads * args.head_dim, bias=False)
+        self.wv = nn.Linear(input_dims=args.dim, output_dims=args.n_kv_heads * args.head_dim, bias=False)
+        self.wo = nn.Linear(input_dims=args.n_heads * args.head_dim, output_dims=args.dim, bias=False)
+        self.rope = RoPE(args.head_dim, traditional=False)
+
+
+
+class MOETransformerBlock(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+        self.n_heads = args.n_heads
+        self.dim = args.dim
+        self.attention = Attention(args=args)
+
+
+
+class Mixtral(nn.Module):
+
+    def __init__(self):
+        super().__init__()
+        self.args = args
+        self.vocab_size = args.vocab_size
+        assert self.vocab_size > 0
+        self.n_layers = args.n_layers
+        self.tok_embeddings = nn.Embedding(self.vocab_size, args.dim)
+        self.layers = [
+            MOETransformerBlock(args=args) for _ in range(self.n_layers)
+        ]
 
 
 class Tokenizer:
@@ -39,6 +91,8 @@ def load_model(folder: str):
     weights = {}
     for wf in weight_files:
         weights.update(mx.load(wf).items())
+    weights = tree_unflatten(list(weights.items()))
+    model = Mixtral(model_args)
 
 
 if __name__ == "__main__":
